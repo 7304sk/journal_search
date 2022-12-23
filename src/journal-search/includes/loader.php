65a9045f7c7fd160798b04cf1bb8ee5f1c685b40
shoalwave <shoalwave.dev@gmail.com>
2022-12-23 17:34:17 +0900

    index.php を整理

--- src/journal-search/includes/loader.php
+++ src/journal-search/includes/loader.php
@@ -1,29 +1,12 @@
 <?php
-/** クラスローダー */
-class ClassLoader {
-    protected $dirs;
+/** config.php の読み込み */
+require_once APP_PATH . 'config.php';
 
-    public function set( $dir ) {
-        $this->dirs[] = $dir;
-    }
-
-    public function start() {
-        spl_autoload_register( array( $this, 'loadClass' ) );
-    }
-
-    public function loadClass( $class ) {
-        foreach( $this->dirs as $dir ) {
-            $file = $dir. '/' . $class . '.php';
-            if( is_readable( $file ) ) {
-                require $file;
-                return;
-            }
-        }
-    }
-}
+/** クラスローダーの実行 */
+require APP_PATH . 'includes/library/ClassLoader.php';
 $loader = new ClassLoader();
 $loader->set( APP_PATH . 'includes/library' );
 $loader->start();
 
-/** 関数の読み込み */
+/** その他ファイルの読み込み */
 require_once APP_PATH . 'includes/functions.php';
